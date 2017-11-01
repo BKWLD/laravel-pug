1c6c3b5e4e7a06412a17c7056b394aa28151650e
KyleKatarn <kylekatarnls@gmail.com>
2017-11-01 15:57:55 +0100

    Use composer plugin system

--- src/UpdateCheck.php
+++ src/UpdateCheck.php
@@ -2,11 +2,38 @@
 
 namespace Bkwld\LaravelPug;
 
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
 use Composer\Json\JsonFile;
+use Composer\Plugin\PluginInterface;
 use Composer\Script\Event;
 
-class UpdateCheck
+class UpdateCheck implements PluginInterface, EventSubscriberInterface
 {
+    protected $io;
+
+    // @codeCoverageIgnoreStart
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $this->io = $io;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            'post-autoload-dump' => array(
+                array('onAutoloadDump', 0),
+            ),
+        );
+    }
+
+    public function onAutoloadDump(Event $event)
+    {
+        static::checkForPugUpgrade($event);
+    }
+    // @codeCoverageIgnoreEnd
+
     private static function getDependencies(Event $event)
     {
         $composer = $event->getComposer();
